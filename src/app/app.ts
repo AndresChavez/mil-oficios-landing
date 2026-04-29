99596cc4c7ca38f65467041d0afb352b9923b6e3
Andres Chavez <andres.chavez23@gmail.com>
2026-04-28 21:38:28 -0500

    Actualizacion Landing Mejorado
    Ajuste de textos en botones de acción. Mejora del SEO.

--- src/app/app.ts
+++ src/app/app.ts
@@ -7,5 +7,5 @@ import { Component, signal } from '@angular/core';
   styleUrl: './app.css'
 })
 export class App {
-  protected readonly title = signal('miloficios-landing');
+  protected readonly title = signal('Mil Oficios');
 }
